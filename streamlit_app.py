f1aa91b53943ab0c92fc1e112aa023255c27e23a
streamlitsnow0405 <153163645+streamlitsnow0405@users.noreply.github.com>
2023-12-07 23:51:12 +0530

    Update streamlit_app.py
    Moved the Fruit Load List Query and Load into a Action Button

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,28 +71,44 @@ except URLError as e:
 
 
 
+
+streamlit.header("THE FRUIT LOAD LIST CONTAINS:")
+#Snowflake Related Function
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+    return my_cur.fetchall()
+    
+#Add a button to load a fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+
+
 # DO NOT RUN ANYTHING POST HERE WHILE TROUBLESHOOTING IS GOING ON
 streamlit.stop()
 
 # import snowflake.connector
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # my_data_row = my_cur.fetchone()
 # streamlit.text("Hello from Snowflake:")
 # streamlit.text(my_data_row)
 
 
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+#my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 #my_data_row = my_cur.fetchone() #fetches only one row
 # streamlit.text("FRUIT LOAD LIST CONTAINS:")
 # streamlit.text(my_data_row)
 
-my_data_rows = my_cur.fetchall()
-streamlit.header("THE FRUIT LOAD LIST CONTAINS:")
-streamlit.dataframe(my_data_rows)
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("THE FRUIT LOAD LIST CONTAINS:")
+#streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
