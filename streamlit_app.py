903cf09acea0b5f7da8a88a99425690fe3a93a5e
streamlitsnow0405 <153163645+streamlitsnow0405@users.noreply.github.com>
2023-12-07 18:09:36 +0530

    Update streamlit_app.py
    added header for new section
    displayed desired response in proper json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected] # only show the filtered ent
 streamlit.dataframe(fruits_to_show)
 
 
+# New Section to display fruityvice API response (fruityvice does not require an API key)
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
