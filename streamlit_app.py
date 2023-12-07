e32703310455451336dc7d15551e0b4351cad03b
streamlitsnow0405 <153163645+streamlitsnow0405@users.noreply.github.com>
2023-12-08 00:16:32 +0530

    Update streamlit_app.py
    Extra Exclamation

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ except URLError as e:
 
 
 
-streamlit.header("View Our Fruit List - Add Your Favorites")
+streamlit.header("View Our Fruit List - Add Your Favorites!!")
 #Snowflake Related Function
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
